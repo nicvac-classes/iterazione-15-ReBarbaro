bd30fe2ae016c5adbcd72bf8583edf09d48f6aa4
ReBarbaro <francescovito.dipalo@marconibari.edu.it>
2024-10-22 17:27:25 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,31 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+   int N,M,i;
+   cout<<"elenca i numeri da N a M con passo 2 "<<endl;
+   cout<<"inserisci N"<<endl;
+   cin>>N;
+   cout<<"inserisci M"<<endl;
+   cin>>M;
+   for (i=M; i<=N; i+=2){
+    cout<<i<<endl;
+    }
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+//ESERCIZIO 2//
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   string nome;
+   int i,N;
+   cout<<"quanti atleti ci sono in gara ?"<<endl;
+   cin>>N;
+   for(i=0;i<=N;i++){
+    cout<<"scrivi il nome dell'atleta"<<endl;
+    cin>>nome;
+    cout<<"ciao"<<nome<<endl;
+    }
+}
